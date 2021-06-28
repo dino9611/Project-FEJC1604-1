709bb87af86146f64140173cbfc4b64810735b35
willy0404 <willybernardus@gmail.com>
2021-06-28 23:32:30 +0700

    PJ-41 addressCart
    Choose address

--- src/pages/users/Cart.jsx
+++ src/pages/users/Cart.jsx
@@ -3,13 +3,13 @@ import Header from '../../components/Header';
 import '../styles/cart.css';
 import { CartAction } from '../../redux/actions';
 import { connect } from 'react-redux';
-import { Table, Container, Modal, ModalBody, ModalHeader, ModalFooter, Collapse, Card, CardBody } from 'reactstrap';
+import { Table, Container, Modal, ModalBody, ModalHeader, ModalFooter, Alert } from 'reactstrap';
 import { API_URL, currencyFormatter } from "../../helper";
 import { FiEdit, FiTrash2 } from 'react-icons/fi';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import Swal from 'sweetalert2';
-import { toast } from 'react-toastify';
+import { toast, Slide, ToastContainer } from 'react-toastify';
 
 
 class Cart extends Component {
@@ -22,18 +22,29 @@ class Cart extends Component {
         qtyInput: 0,
         ordersdetail_id: 0,
         addresses: [],
-        isOpen: false
+        selected_address: {
+            address: "",
+            city: "",
+            zip: "",
+        },
+        modalAddress: false
     };
 
     componentDidMount() {
+        // console.log(this.props.dataUser);
         axios.get(`${API_URL}/auth/address/${this.props.dataUser.id}`)
             .then((res) => {
-                console.log('ini res.data', res.data);
-                this.setState({ addresses: res.data });
+                console.log('ini addresses', res.data);
+                this.setState({ addresses: res.data, selected_address: res.data[0] });
+                console.log('ini selected_address', this.state.selected_address);
             }).catch((error) => {
                 console.error(error);
             });
     }
+
+    toggleAddress = () => {
+        this.setState({ modalAddress: !this.state.modalAddress });
+    };
 
     toggle = () => {
         this.setState({ modalVisible: !this.state.modalVisible, qtyInput: 0, productName: "", ordersdetail_id: 0 });
@@ -102,7 +113,7 @@ class Cart extends Component {
             showCancelButton: true,
             confirmButtonColor: '#3085d6',
             cancelButtonColor: '#d33',
-            confirmButtonText: 'Yes, delete it!'
+            confirmButtonText: 'Delete'
         }).then((result) => {
             if (result.isConfirmed) {
                 let ordersdetail_id = this.props.dataUser.cart[index].ordersdetail_id;
@@ -139,7 +150,6 @@ class Cart extends Component {
                         <FiEdit onClick={() => this.toggleEdit(val)} className="edit-btn" />
                         <FiTrash2 onClick={() => this.deleteItemClick(index)} className="delete-btn" />
                     </td>
-
                 </tr>
             );
         });
@@ -157,29 +167,53 @@ class Cart extends Component {
         this.setState({ [e.target.name]: e.target.value });
     };
 
-    renderAddress = () => {
+    addressOption = () => {
         return this.state.addresses.map((val, index) => {
             return (
-                <p>
-                    {val.address}
-                    <br />
-                    {val.zip}
-                    <br />
-                    {val.city}
-                </p>
-
+                <div key={index} style={{ color: 'gray', display: 'flex' }}>
+                    <div style={{ flex: 1 }}>
+                        <div> {val.address}</div>
+                        <div>{val.city + ', ' + val.zip}</div>
+                        <div style={{ color: "#89ADC3" }}>{val.is_default ? '(Default address)' : null}</div>
+                        <br />
+                    </div>
+                    <div>
+                        {this.state.selected_address.id === this.state.addresses[index].id ?
+                            <div style={{ color: "#052C43", marginRight: '10px', marginTop: '15px' }}>Selected</div>
+                            :
+                            <button
+                                style={{ flex: 1, justifyContent: "space-between" }}
+                                className="modal-btn1"
+                                onClick={() => this.selectAddressClick(index)}
+                            >
+                                Select
+                            </button>
+                        }
+                    </div>
+                </div>
             );
         });
-
     };
 
-    isOpen = () => {
-        this.setState({ isOpen: !this.state.isOpen });
+    selectAddressClick = (index) => {
+        // ketika tekan select akan mengubah this.state.selected_address dengan this.state.addresses[index]
+        this.setState({ selected_address: this.state.addresses[index], modalAddress: !this.state.modalAddress });
+        toast.dark('Address Successfuly Changed', {
+            position: "top-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: false,
+            draggable: true,
+            progress: undefined,
+            transition: Slide
+        });
     };
 
     render() {
         return (
             <div>
+                {/* Modal QTY */}
                 <Modal isOpen={this.state.modalVisible} toggle={this.toggle} centered>
                     <ModalHeader>Edit Qty {this.state.productName}</ModalHeader>
                     <ModalBody>
@@ -199,6 +233,18 @@ class Cart extends Component {
                         <button className="modal-btn2" onClick={this.toggle}>Cancel</button>
                     </ModalFooter>
                 </Modal>
+                {/* Modal Address */}
+                <Modal isOpen={this.state.modalAddress} toggle={this.toggleAddress} centered >
+                    <ModalHeader className="modaladd-font">
+                        Select Shipment Address
+                    </ModalHeader>
+                    <ModalBody>
+                        {this.addressOption()}
+                    </ModalBody>
+                </Modal>
+                <Modal>
+                    <ModalHeader>Payment</ModalHeader>
+                </Modal>
                 <div className="cart-background">
                     <Header />
                     <div className="section-content">
@@ -206,18 +252,23 @@ class Cart extends Component {
                     </div>
                 </div>
                 <Container>
-                    <h3 style={{ marginTop: "35px" }}>Alamat Tujuan:</h3>
-                    {this.renderAddress()}
-                    <Collapse >
-                        <Card>
-                            <CardBody>
-                                Anim pariatur cliche reprehenderit,
-                                enim eiusmod high life accusamus terry richardson ad squid. Nihil
-                                anim keffiyeh helvetica, craft beer labore wes anderson cred
-                                nesciunt sapiente ea proident.
-                            </CardBody>
-                        </Card>
-                    </Collapse>
+                    <h3 style={{ marginTop: "35px" }}>Shipping Address:</h3>
+                    <div className="alamat-box" >
+                        <div style={{ fontWeight: "700" }}>
+                            {this.props.dataUser.first_name + ' ' + this.props.dataUser.last_name}
+                        </div>
+                        {this.props.dataUser.phone_number}
+                        <br />
+                        <div style={{ color: 'gray' }}>
+                            {this.state.selected_address.address}
+                            <br />
+                            {this.state.selected_address.city + ', ' + this.state.selected_address.zip}
+                        </div>
+                        <div style={{ color: "#89ADC3" }}>
+                            {this.state.selected_address.is_default ? 'Default address' : null}
+                        </div>
+                    </div>
+                    <button className="other-address" onClick={this.toggleAddress}>Choose Other Address</button>
                     <Table bordered hover className="table-margin">
                         <tr className="text-center">
                             <th>No</th>
@@ -243,10 +294,10 @@ class Cart extends Component {
                         </tbody>
                     </Table>
                     {/* <Link to='/checkout'> */}
-                    <button className="checkout-btn">Check Out</button>
+                    <button className="checkout-btn">Choose Payment</button>
                     {/* </Link> */}
                 </Container>
-            </div>
+            </div >
         );
     }
 }
